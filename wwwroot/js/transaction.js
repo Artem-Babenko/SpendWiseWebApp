dffe11035eaf9275b56be77a424254c30c8711b3
Babenko Artem <babenkoartem505@gmail.com>
2024-01-26 13:12:00 +0200

    Fix transaction list sorting.

--- wwwroot/js/transaction.js
+++ wwwroot/js/transaction.js
@@ -1,28 +1,21 @@
 
 const searchInput = document.getElementById('search');
-const transactionElements = document.querySelectorAll('table tbody tr');
-const typeSelect = document.querySelector('.sortbar .type');
-const categorySelect = document.querySelector('.sortbar .category');
+const transactionElements = document.querySelectorAll('.scroll-space table tbody tr');
+const typeSelect = document.querySelector('.sortbar select.type');
+const categorySelect = document.querySelector('.sortbar select.category');
+
+// Variables for sorting.
+let searchTerm = "";
+let choosedCategory = "default";
+let choosedType = "any";
 
 // The 'input' event on the 'searchInput' element.
 searchInput.addEventListener('input', () => {
     // Get the trimmed and lowercase version of the search term.
-    const searchTerm = searchInput.value.trim().toLowerCase();
-
-    // Loop through each transaction element in the table.
-    for (const element of transactionElements) {
-        // Get the lowercase version of the text content of the 'title' element within the current transaction.
-        const title = element.querySelector('.title').textContent.toLowerCase();
-        // Check if the title includes the search term.
-        if (title.includes(searchTerm))
-            element.style.display = "table-row";
-        else
-            element.style.display = "none";
-    }
+    searchTerm = searchInput.value.trim().toLowerCase();
+    updateTransactions();
 });
 
-// When selecting a transaction type,
-// the list of possible categories for selection is updated.
 typeSelect.addEventListener('change', function () {
     var selectedValue = this.value;
 
@@ -39,31 +32,44 @@ typeSelect.addEventListener('change', function () {
         }
     }
 
-    // Loop through each transaction element in the table.
-    for (const transactionElement of transactionElements) {
-        // Check if the selected type is 'any' or if the transaction has the selected category.
-        const isMatchingCategory = selectedValue === "any" ||
-            transactionElement.querySelector('.' + selectedValue);
-        // Set the display property of the transaction element based on the category match.
-        transactionElement.style.display = isMatchingCategory ? "table-row" : "none";
-    }
-
     // Set the default option as selected in the 'categotySelect' dropdown.
     categorySelect.querySelector('option.default').selected = true;
+
+    // Update transactions list.
+    choosedType = selectedValue;
+    choosedCategory = "default";
+    updateTransactions();
 });
 
-// When selecting a transaction category,
-// the list of transaction is updated.
+// If choose category, the transaction list will be updated.
 categorySelect.addEventListener('change', function () {
-    var selectedValue = this.value;
-
-    // Loop through each transaction element.
-    for (const transactionElement of transactionElements) {
-        // Get the category text content of the current transaction element.
-        const categoryText = transactionElement.querySelector('.category').textContent;
-        // Check if the selected value is "default" or matches the category of the transaction.
-        const isMatch = selectedValue === "default" || categoryText === selectedValue;
-        // Set the display style based on whether it's a match or not.
-        transactionElement.style.display = isMatch ? "table-row" : "none";
-    }
+    choosedCategory = this.value;
+    updateTransactions();
 });
+
+// Function of updating the list on transaction according to sorting.
+function updateTransactions() {
+    // Take each row of the list and check whether it corresponds to the sorting.
+    for (transactionElement of transactionElements) {
+        // Display all transactions.
+        transactionElement.style.display = "table-row";
+        const typeIsIncome = transactionElement.querySelector('.income') ? true : false;
+
+        // Type sorting.
+        if (choosedType === "income") {
+            if (!typeIsIncome) transactionElement.style.display = "none";
+        }
+        else if (choosedType === "expense") {
+            if (typeIsIncome) transactionElement.style.display = "none";
+        }    
+
+        // Category sorting.
+        const categoryText = transactionElement.querySelector('.category').textContent;
+        const isMatch = choosedCategory === "default" || categoryText === choosedCategory;
+        if (!isMatch) transactionElement.style.display = "none";
+
+        // Search term sorting.
+        const title = transactionElement.querySelector('.title').textContent.toLowerCase();
+        if (!title.includes(searchTerm)) transactionElement.style.display = "none";
+    }    
+}
